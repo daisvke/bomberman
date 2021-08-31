9783494bfedd4e3706a1cd4a7b01e6c63f6f723c
dtanigaw <dtanigaw@student.42.fr>
2021-08-31 05:37:49 +0200

    Check what has to be turned off on each direction after explosion

--- src/set_bomb.c
+++ src/set_bomb.c
@@ -6,13 +6,13 @@
 /*   By: dtanigaw <dtanigaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/17 20:21:03 by dtanigaw          #+#    #+#             */
-/*   Updated: 2021/08/28 13:24:19 by dtanigaw         ###   ########.fr       */
+/*   Updated: 2021/08/31 05:30:43 by dtanigaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
-void	sl_find_which_ennemy_is_dead(t_env *env, int x_start, int y)
+void	sl_find_which_ennemy_is_dead(t_env *env, int x, int y)
 {
 	int			ennemies_count;
 	int			i;
@@ -21,14 +21,38 @@ void	sl_find_which_ennemy_is_dead(t_env *env, int x_start, int y)
 	i = 0;
 	while (i < ennemies_count)
 	{
-		if (env->tex.ennemies.sprites[i].pos.x == x_start && env->tex.ennemies.sprites[i].pos.y == y)
+		if (env->tex.ennemies.sprites[i].pos.x == x && env->tex.ennemies.sprites[i].pos.y == y)
 			env->tex.ennemies.sprites[i].alive = false;
 		++i;
 	}
-	env->map[y][x_start] = MAP_FLOOR;
+	env->map[y][x] = MAP_FLOOR;
 }
 
-bool	sl_check_if_sprite_is_dead(t_env *env, t_items *bomb, char *map[], int  x, int y)
+void	sl_check_each_element(t_env *env, int x, int y)
+{
+	char	**map;
+	int		collectible;
+
+	map = env->map;
+	if (map[y][x] == MAP_PLAYER)
+		env->p1.alive = false;
+	if (map[y][x] == MAP_ENNEMY)
+		sl_find_which_ennemy_is_dead(env, x, y);
+	collectible = sl_is_collectible(map[y][x]);
+	if (map[y][x] != MAP_WALL)
+		map[y][x] = MAP_FLOOR;
+	if (collectible == 1)
+	{
+		sl_find_and_turn_off_item(env->tex.bomb.item_bombs, env->tex.bomb.to_collect, x, y);
+		--env->tex.bomb.to_collect;
+	}
+	if (collectible == 2)
+		sl_find_and_turn_off_item(env->tex.fire.items, env->tex.fire.to_collect, x, y);
+	if (collectible == 3)
+		sl_find_and_turn_off_item(env->tex.speed.items, env->tex.speed.to_collect, x, y);
+}
+
+bool	sl_check_what_is_affected_by_the_explosion(t_env *env, t_items *bomb, char *map[], int  x, int y)
 {
 	int	p1_x;
 	int	p1_y;
@@ -37,61 +61,44 @@ bool	sl_check_if_sprite_is_dead(t_env *env, t_items *bomb, char *map[], int  x, 
 	int	y_start;
 	int	y_end;
 	int	size;
-	int	collectible;
 
 	size = bomb->explode_size;
 	p1_x = env->p1.pos.x;
 	p1_y = env->p1.pos.y; 
-	x_start = x - size;
-	if (x_start < 0)
-		x_start = 0;
+	x_start = x;
+	x_end = x - size;
+	if (x_end < 0)
+		x_end = 0;
+	while (x_start > x_end && map[y][x_start] != MAP_WALL)
+	{
+		sl_check_each_element(env, x_start, y);
+		--x_start;
+	}
+	x_start = x;
 	x_end = x + size;
 	if (x_end > env->width)
 		x_end = env->width;
-	y_start = y - size;
-	if (y_start < 0)
-		y_start = 0;
+	while (x_start < x_end && map[y][x_start] != MAP_WALL)
+	{
+		sl_check_each_element(env, x_start, y);
+		++x_start;
+	}
+	y_start = y;
+	y_end = y - size;
+	if (y_end < 0)
+		y_end = 0;
+	while (y_start > y_end && map[y_start][x] != MAP_WALL)
+	{
+		sl_check_each_element(env, x, y_start);
+		--y_start;
+	}
+	y_start = y;
 	y_end = y + size;
 	if (y_end > env->height)
 		y_end = env->height;
-	while (x_start < x_end)
+	while (y_start < y_end && map[y_start][x] != MAP_WALL)
 	{
-		if (map[y][x_start] == MAP_PLAYER)
-			return (true);
-		if (map[y][x_start] == MAP_ENNEMY)
-			sl_find_which_ennemy_is_dead(env, x_start, y);
-		collectible = sl_is_collectible(map[y][x_start]);
-		if (map[y][x_start] != MAP_WALL)
-			map[y][x_start] = MAP_FLOOR;
-		if (collectible == 1)
-		{
-			sl_find_and_turn_off_item(env->tex.bomb.item_bombs, env->tex.bomb.to_collect, x_start, y);
-			--env->tex.bomb.to_collect;
-		}
-		if (collectible == 2)
-			sl_find_and_turn_off_item(env->tex.fire.items, env->tex.fire.to_collect, x_start, y);
-		if (collectible == 3)
-			sl_find_and_turn_off_item(env->tex.speed.items, env->tex.speed.to_collect, x_start, y);
-		++x_start;
-	}
-	while (y_start < y_end)
-	{
-		if (map[y_start][x] == MAP_PLAYER)
-			return (true);
-		if (map[y_start][x] == MAP_ENNEMY)
-			sl_find_which_ennemy_is_dead(env, x_start, y);
-		collectible = sl_is_collectible(map[y_start][x]);
-		if (map[y_start][x] != MAP_WALL)
-			map[y_start][x] = MAP_FLOOR;
-		if (collectible == 1)
-		{
-			--env->tex.bomb.to_collect;
-			sl_find_and_turn_off_item(env->tex.bomb.item_bombs, env->tex.bomb.to_collect, x, y_start);
-		}
-		if (collectible == 2)
-			sl_find_and_turn_off_item(env->tex.fire.items, env->tex.fire.to_collect, x, y_start);
-		if (collectible == 3)
-			sl_find_and_turn_off_item(env->tex.speed.items, env->tex.speed.to_collect, x, y_start);
+		sl_check_each_element(env, x, y_start);
 		++y_start;
 	}
 	return (false);
@@ -103,7 +110,7 @@ void	sl_explode_bomb(t_env *env, t_items *bomb, int x, int y)
 
 	if (bomb->time3 <= BOMB_EXPLODE_TIME)
 	{
-		is_dead = sl_check_if_sprite_is_dead(env, bomb, env->map, x / BLOC_LEN, y / BLOC_LEN);
+		is_dead = sl_check_what_is_affected_by_the_explosion(env, bomb, env->map, x / BLOC_LEN, y / BLOC_LEN);
 		sl_draw_segments_of_exploding_bomb(env, bomb, x, y);
 		if (is_dead)
 			env->p1.alive = false;
